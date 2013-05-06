8a1ce4c7cf4d250226e52351408dc28e5d789169
gtt116 <gtt116@gmail.com>
2013-05-06 13:58:24 +0800

    Rename to pync

--- pync.py
+++ pync.py
@@ -18,18 +18,17 @@ def parse(setup):
     return settings
 
 def server(*settings):
-    try:
-        dock_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        dock_socket.bind(('', settings[2]))
-        dock_socket.listen(5)
-        while True:
-            client_socket = dock_socket.accept()[0]
-            server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            server_socket.connect((settings[0], settings[1]))
-            thread.start_new_thread(forward, (client_socket, server_socket))
-            thread.start_new_thread(forward, (server_socket, client_socket))
-    finally:
-        thread.start_new_thread(server, settings)
+    dock_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    dock_socket.bind(('', settings[2]))
+    dock_socket.listen(5)
+    while True:
+        client_socket = dock_socket.accept()[0]
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_socket.connect((settings[0], settings[1]))
+        thread.start_new_thread(forward, (client_socket, server_socket))
+        thread.start_new_thread(forward, (server_socket, client_socket))
+#    finally:
+#        thread.start_new_thread(server, settings)
 
 def forward(source, destination):
     string = ' '
